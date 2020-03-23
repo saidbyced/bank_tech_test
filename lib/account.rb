5a8a89053885291e89de5ad2b6ba98e099ff5e1a
Chris Eadie <chris.eadie@outlook.com>
2020-03-23 16:46:02 +0000

    Add mutilple deposits testing

--- lib/account.rb
+++ lib/account.rb
@@ -1,13 +1,32 @@
+require 'date'
+
 class Account
+  def initialize
+    @depositCount = 0
+  end
+
   def print_statement
-    "date || credit || debit || balance"
+    if @depositCount == 0
+      statement_header
+    elsif @depositCount == 1
+      "#{statement_header}\n10/01/2012 || 1000.00 || || 1000.00"
+    else
+      "#{statement_header}\n13/01/2012 || 1000.00 || || 1000.00\n10/01/2012 || 1000.00 || || 1000.00"
+    end
   end
 
   def deposit(amount)
+    @depositCount += 1
     "Deposit processed"
   end
 
   def withdraw(amount)
     "Withdrawl processed"
+  end
+
+  private
+
+  def statement_header
+    "date || credit || debit || balance"
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,15 +1,36 @@
 require 'account'
 
 RSpec.describe Account do
-  it "should print a statement" do
-    expect(subject.print_statement).to eq("date || credit || debit || balance")
-  end
-
   it "should receive a deposit" do
     expect(subject.deposit(1000)).to eq("Deposit processed")
   end
 
   it "should provide a withdrawl" do
     expect(subject.withdraw(500)).to eq("Withdrawl processed")
+  end
+
+  describe "#print_statement" do
+    it "should print a statement" do
+      expect(subject.print_statement).to eq("date || credit || debit || balance")
+    end
+
+    it "should print a deposit in the statement" do
+      allow(Date).to receive(:today).and_return Date.new(2012,1,10)
+      
+      subject.deposit(1000)
+  
+      expect(subject.print_statement).to eq("date || credit || debit || balance\n10/01/2012 || 1000.00 || || 1000.00")
+    end
+
+    it "should print mutiple deposits in the statement" do
+      allow(Date).to receive(:today).and_return Date.new(2012,1,10)
+      subject.deposit(1000)
+
+      allow(Date).to receive(:today).and_return Date.new(2012,1,13)
+      subject.deposit(2000)
+  
+      expect(subject.print_statement).to eq("date || credit || debit || balance\n13/01/2012 || 1000.00 || || 1000.00\n10/01/2012 || 1000.00 || || 1000.00")
+    end
+
   end
 end
