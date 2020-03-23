6760ef5379f6cb93c711347deea4df9f15c3ffbb
Chris Eadie <chris.eadie@outlook.com>
2020-03-23 17:35:46 +0000

    Refactor process

--- lib/account.rb
+++ lib/account.rb
@@ -2,31 +2,34 @@ require 'date'
 
 class Account
   def initialize
-    @depositCount = 0
+    @balance = 0
+    @transactions = []
   end
 
   def print_statement
-    if @depositCount == 0
-      statement_header
-    elsif @depositCount == 1
-      "#{statement_header}\n10/01/2012 || 1000.00 || || 1000.00"
-    else
-      "#{statement_header}\n13/01/2012 || 1000.00 || || 1000.00\n10/01/2012 || 1000.00 || || 1000.00"
-    end
+    return statement
   end
 
-  def deposit(amount)
-    @depositCount += 1
-    "Deposit processed"
+  def deposit(credit)
+    date = Date.today.strftime("%d/%m/%Y")
+
+    @balance += credit
+    @transactions.unshift("\n#{date} || #{credit}.00 || || #{@balance}.00")
+    return "Deposit processed"
   end
 
   def withdraw(amount)
-    "Withdrawl processed"
+    return "Withdrawl processed"
   end
 
   private
 
-  def statement_header
-    "date || credit || debit || balance"
+  def statement
+    statement = "date || credit || debit || balance"
+    @transactions.each do |transaction|
+      statement += transaction
+    end
+    
+    return statement
   end
 end
